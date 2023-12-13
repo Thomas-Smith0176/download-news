17b0ef2ab9a3988aa937b653ef30f9ccdea7090b
Thomas-Smith0176 <thomas_smith@talktalk.net>
2023-12-13 12:14:28 +0000

    fixes issues with users posting empty comments

--- src/components/CommentAdder.jsx
+++ src/components/CommentAdder.jsx
@@ -11,30 +11,40 @@ const CommentAdder = ({setComments}) => {
 
     const [newComment, setNewComment] = useState('');
     const [err, setErr] = useState(null)
+    const [tempId, setTempId] = useState(0)
 
     function handlePostComment() {
-        const timePosted = new Date();
-        const newCommentObj = {
-            author: currUser.username,
-            body: newComment,
-            createdAt: parseDate(timePosted),
-            votes: 0
+        if (newComment.length === 0) {
+            return <p></p>
         }
-        setComments((currComments) => [newCommentObj, ...currComments])
-        postComment(article_id, currUser.username, newComment).catch(() => {
-            setComments((currComments) => currComments.shift())
-            setErr('Something went wrong! Please try that again')
-        })
+        else {
+            setNewComment('')
+            setTempId((currTempId) => currTempId - 1)
+            const timePosted = new Date();
+            const newCommentObj = {
+                comment_id: tempId,
+                author: currUser.username,
+                body: newComment,
+                createdAt: parseDate(timePosted),
+                votes: 0
+            }
+            setComments((currComments) => [newCommentObj, ...currComments])
+            postComment(article_id, currUser.username, newComment).catch(() => {
+                setComments((currComments) => currComments.shift())
+                setErr('Something went wrong! Please try that again')
+            })
+        }
     };
 
     return (
         <>
-        <Form>
+        {!currUser && <p>Please sign in to post a comment</p>}
+        {currUser && <Form>
             <Form.Label className="comment-label">Post a comment</Form.Label>
             <Form.Control type="text" placeholder="new comment..." value={newComment} onChange={(event) => setNewComment(event.target.value)}></Form.Control>
             {err && <p>{err}</p>}
             <Button onClick={() => {handlePostComment()}}>Post</Button>
-        </Form>
+        </Form>}
         </>
     )
 };
